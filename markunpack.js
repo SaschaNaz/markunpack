b7d14bed2cad98bb84a4e078c10363cd9cdeb93d
SaschaNaz <saschanaz@outlook.com>
2014-10-24 00:30:24 +0900

    basic form

--- markunpack.js
+++ markunpack.js
@@ -4,9 +4,47 @@
 * Use JSZip, replace local image URLs to blob URLs
 * Bootstrap?
 */
-var MarkUnpack = (function () {
-    function MarkUnpack() {
+var MarkUnpack;
+(function (MarkUnpack) {
+    function readFileAsArrayBuffer(blob) {
+        return new Promise(function (resolve, reject) {
+            var reader = new FileReader();
+            reader.onload = function () {
+                return resolve(reader.result);
+            };
+            reader.onerror = function (ev) {
+                return reject(ev);
+            };
+            reader.readAsArrayBuffer(blob);
+        });
     }
-    return MarkUnpack;
-})();
+    function readFileAsText(blob) {
+        return new Promise(function (resolve, reject) {
+            var reader = new FileReader();
+            reader.onload = function () {
+                return resolve(reader.result);
+            };
+            reader.onerror = function (ev) {
+                return reject(ev);
+            };
+            reader.readAsText(blob);
+        });
+    }
+    function unseal(blob) {
+        return readFileAsText(blob).then(function (text) {
+            return marked(text, { gfm: true });
+        });
+    }
+    MarkUnpack.unseal = unseal;
+    function unpack(blob) {
+        return readFileAsArrayBuffer(blob).then(function (arraybuffer) {
+            var jszip = new JSZip(arraybuffer);
+            var main = jszip.file("index.md");
+            if (!main)
+                throw new Error("No index.md file in this markpack.");
+            return marked(main.asText(), { gfm: true });
+        });
+    }
+    MarkUnpack.unpack = unpack;
+})(MarkUnpack || (MarkUnpack = {}));
 //# sourceMappingURL=markunpack.js.map
--- markunpack.ts
+++ markunpack.ts
@@ -5,6 +5,37 @@
  * Bootstrap?
  */
 
-class MarkUnpack {
-
+module MarkUnpack {
+    function readFileAsArrayBuffer(blob: Blob) {
+        return new Promise<ArrayBuffer>((resolve, reject) => {
+            var reader = new FileReader();
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = (ev) => reject(ev);
+            reader.readAsArrayBuffer(blob);
+        });
+    }
+    function readFileAsText(blob: Blob) {
+        return new Promise<string>((resolve, reject) => {
+            var reader = new FileReader();
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = (ev) => reject(ev);
+            reader.readAsText(blob);
+        });
+    }
+    export function unseal(blob: Blob): Promise<string> {
+        return readFileAsText(blob)
+            .then((text) => {
+                return marked(text, { gfm: true });
+            });
+    }
+    export function unpack(blob: Blob): Promise<string> {
+        return readFileAsArrayBuffer(blob)
+            .then((arraybuffer) => {
+                var jszip = new JSZip(arraybuffer);
+                var main = jszip.file("index.md");
+                if (!main)
+                    throw new Error("No index.md file in this markpack.");
+                return marked(main.asText(), { gfm: true });
+            });
+    }
 }
